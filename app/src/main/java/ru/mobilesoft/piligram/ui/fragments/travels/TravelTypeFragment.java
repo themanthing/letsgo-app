e1bd1d25816915870eb016eed75c3f7de529877b
user32338 <1857061@gmail.com>
2017-09-11 19:51:24 +0300

    add custom radio button

--- app/src/main/java/ru/mobilesoft/piligram/ui/fragments/travels/TravelTypeFragment.java
+++ app/src/main/java/ru/mobilesoft/piligram/ui/fragments/travels/TravelTypeFragment.java
@@ -3,15 +3,24 @@ package ru.mobilesoft.piligram.ui.fragments.travels;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.ViewGroup;
 
+import butterknife.BindView;
 import ru.mobilesoft.piligram.R;
 import ru.mobilesoft.piligram.ui.fragments.BaseWizardFragment;
+import ru.mobilesoft.piligram.ui.widget.CustomRadioButton;
 
 /**
  * Created on 10.09.17.
  */
 
 public class TravelTypeFragment extends BaseWizardFragment {
+
+    @BindView(R.id.select_group)
+    ViewGroup selectGroup;
+
+    private int selectedId = View.NO_ID;
+
     @Override
     protected int getLayout() {
         return R.layout.fragment_travel_type;
@@ -20,7 +29,20 @@ public class TravelTypeFragment extends BaseWizardFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setActionButtonEnabled(true);
+        for (int i = 0; i < selectGroup.getChildCount(); i++) {
+            selectGroup.getChildAt(i).setOnClickListener(v -> {
+
+                selectedId = v.getId();
+
+                for (int k = 0; k < selectGroup.getChildCount(); k++) {
+                    CustomRadioButton radioButton = (CustomRadioButton) selectGroup.getChildAt(k);
+                    radioButton.setChecked(radioButton.getId() == selectedId);
+                }
+
+                setActionButtonEnabled(selectedId != View.NO_ID);
+
+            });
+        }
     }
 
     @Override
--- app/src/main/java/ru/mobilesoft/piligram/ui/widget/CustomRadioButton.java
+++ app/src/main/java/ru/mobilesoft/piligram/ui/widget/CustomRadioButton.java
@@ -1,9 +1,18 @@
 package ru.mobilesoft.piligram.ui.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import ru.mobilesoft.piligram.R;
 
@@ -12,6 +21,11 @@ import ru.mobilesoft.piligram.R;
  */
 
 public class CustomRadioButton extends FrameLayout {
+
+    private ImageView icon;
+    private TextView title;
+    private RadioButton radio;
+    private ViewGroup rootView;
 
     public CustomRadioButton(Context context) {
         super(context);
@@ -30,9 +44,48 @@ public class CustomRadioButton extends FrameLayout {
 
     private void init(@Nullable AttributeSet attrs) {
         inflate(getContext(), R.layout.widget_radio_button, this);
-
+        initView();
         if (attrs != null) {
-
+            TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.CustomRadioButton);
+            try {
+                setTitle(array.getString(R.styleable.CustomRadioButton_android_text));
+                setIcon(array.getResourceId(R.styleable.CustomRadioButton_icon, R.drawable.ski));
+            }finally {
+                array.recycle();
+            }
         }
     }
+
+    private void initView(){
+        icon = (ImageView) findViewById(R.id.icon);
+        title = (TextView) findViewById(R.id.title);
+        radio = (RadioButton) findViewById(R.id.radio);
+        rootView = (ViewGroup) findViewById(R.id.rootView);
+    }
+
+    public void setIcon(@DrawableRes int icon){
+        this.icon.setImageResource(icon);
+    }
+
+    public void setTitle(CharSequence title){
+        this.title.setText(title);
+    }
+
+    public boolean isCheced(){
+        return radio.isChecked();
+    }
+
+    public void setChecked(boolean checked){
+        radio.setChecked(checked);
+    }
+
+    @Override
+    public void setOnClickListener(@Nullable OnClickListener l) {
+        rootView.setOnClickListener(v -> {
+            if (l != null){
+                l.onClick(CustomRadioButton.this);
+            }
+        });
+    }
+
 }
